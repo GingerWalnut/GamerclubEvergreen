02f17a51d9afe994e91ab776096c0f3b090d5827
Sea <Nickbrock778@gmail.com>
2025-12-01 19:57:55 -0500

    Added Minecolonies, QoL Mods
    added create log recipe fix made by hazel

---
+++ overrides/kubejs/server_scripts/create_logs.js
@@ -0,0 +1,26 @@
+ServerEvents.tags('item', event => {
+  event.add('c:stripped_woods', 'vanillabackport:stripped_pale_oak_log')
+  event.add('c:stripped_woods', 'vanillabackport:stripped_pale_oak_wood')
+
+  event.add('c:stripped_woods', 'aether:stripped_skyroot_log')
+  event.add('c:stripped_woods', 'aether:stripped_skyroot_wood')
+
+  event.add('c:stripped_woods', 'deeperdarker:stripped_echo_log')
+  event.add('c:stripped_woods', 'deeperdarker:stripped_echo_wood')
+
+  event.add('c:stripped_woods', 'hexalia:stripped_cottonwood_log')
+  event.add('c:stripped_woods', 'hexalia:stripped_cottonwood_wood')
+
+  event.add('c:stripped_woods', 'hexalia:stripped_willow_log')
+  event.add('c:stripped_woods', 'hexalia:stripped_willow_wood')
+
+  event.add('c:stripped_woods', 'malum:stripped_runewood_log')
+  event.add('c:stripped_woods', 'malum:stripped_runewood')
+
+  event.add('c:stripped_woods', 'malum:stripped_soulwood_log')
+  event.add('c:stripped_woods', 'malum:stripped_souldwood')
+
+  console.log('Added create tags to logs')
+})
+
+
